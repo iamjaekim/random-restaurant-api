ca9ee40ebde36ac244f0dab1ddce4b23f8b09a7f
Jae Wook Kim <jipod30@hotmail.com>
2020-05-25 03:35:45 -0400

    app

--- api.py
+++ api.py
@@ -52,4 +52,5 @@ def get_restaurant(storeId):
         abort(404,description="Yelp API Error")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
